80b76c814935be9b17415595d65aa652515c25d5
Pamela Rodrigues <junmyeonradpet@gmail.com>
2025-04-12 00:50:42 -0300

    mais uma

--- lista6_beecrowd/q11.c
+++ lista6_beecrowd/q11.c
@@ -1,39 +1,59 @@
-//one-two-three
+// one-two-three
+//Uso obrigatório de enum para representar os números e 
+// uma struct para armazenar a palavra e sua correspondência numérica.
 #include <stdio.h>
 #include <string.h>
- 
+
+typedef enum {
+    UM =1,
+    DOIS,
+    TRES
+} Numeros;
+
+typedef struct
+{
+    Numeros n;
+    char palavra_errada[10];
+} Palavra;
+
+Numeros verificar_palavra(Palavra palavrinha){
+    int um = 0;
+    if (strlen(palavrinha.palavra_errada) == 5) {
+        // printf("3\n");
+        return TRES;
+
+    } else if (strlen(palavrinha.palavra_errada) == 3) {
+        if (palavrinha.palavra_errada[0] == 'o'){
+            um++;
+        } 
+        if (palavrinha.palavra_errada[1] == 'n'){
+            um++;
+        } 
+        if (palavrinha.palavra_errada[2] == 'e'){
+            um++;
+        }
+
+        if (um >= 2) {
+            // printf("1\n");
+            return UM;
+        } else {
+            // printf("2\n");
+            return DOIS;
+        }
+    }   
+}
+
 int main() {
     int casos;
-    char palavra_fornecida[11];
+    Palavra palavra_fornecida;
     
-    // three = 5
-    // one e two = 2
-    int um = 0;
-    int dois = 0;
-    for (int i = 0; i < casos; i++){
-        scanf("%s", palavra_fornecida);
-        
-        if(sizeof(palavra_fornecida) == 5){
-            printf("3\n");
-        } else (sizeof(palavra_fornecida == 2)){
-            for (int i = 0; i < sizeof(palavra_fornecida); i++){
-                if(strcmp(palavra_fornecida[i], "o") == 1){
-                    um++;
-                }
-                if (strcmp(palavra_fornecida[i], "n") == 1){
-                    um++;
-                }
-                if (strcmp(palavra_fornecida[i], "e") == 1){
-                    um++;
-                }
-            }
-            if (um >= 2){
-                printf("1\n");
-            } else {
-                printf("2\n");
-            }
-        }
+    scanf("%d", &casos);
+    Numeros r;
+    for (int i = 0; i < casos; i++) {
+        scanf("%s", palavra_fornecida.palavra_errada);
+        r = verificar_palavra(palavra_fornecida);
+        printf("%d\n", r);
     }
- 
+
     return 0;
 }
